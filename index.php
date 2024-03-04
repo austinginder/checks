ecc5015777e00eede491a81f63ae453aa71a5c5b
Austin Ginder <austinginder@gmail.com>
2024-03-04 14:21:32 -0500

    👌 IMPROVE: PHP server

---
+++ index.php
@@ -0,0 +1,44 @@
+<?php
+
+require_once "vendor/autoload.php";
+
+date_default_timezone_set('UTC');
+
+include("lib/rotate-images.php");
+include("lib/check-generator.php");
+
+$CHK = new CheckGenerator;
+
+$check['logo']           = "";
+$check['from_name']      = "Your Name";
+$check['from_address1']  = "1234 E Main St";
+$check['from_address2']  = "Portland, OR 97214";
+$check['routing_number'] = "123000220";
+$check['account_number'] = "123456789012";
+$check['bank_1']         = "US Bank";
+$check['bank_2']         = "1225 SE Cesar E Chavez Blvd";
+$check['bank_3']         = "Portland, OR 97214-4371";
+$check['bank_4']         = "(503) 275-4550";
+$check['signature']      = "";
+$check['pay_to']         = "";
+$check['amount']         = '';
+$check['date']           = "";
+$check['memo']           = "";
+$check['check_number']   = 1000;
+
+$CHK->AddCheck($check);
+
+if(array_key_exists('REMOTE_ADDR', $_SERVER)) {
+  // Called from a browser
+  header('Content-Type: application/octet-stream', false);
+  header('Content-Type: application/pdf', false);
+  $CHK->PrintChecks();
+} else {
+  // Called from the command line
+  ob_start();
+  $CHK->PrintChecks();
+  $pdf = ob_get_clean();
+  file_put_contents('checks.pdf', $pdf);
+  echo "Saved to file: checks.pdf\n";
+}
+
